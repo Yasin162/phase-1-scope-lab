460c988cfcd39575862297395b689938c70d6d4d
Yasin <yasinabdussaburr@gmail.com>
2021-09-07 09:22:15 -0400

    commit tree 3a1e0b0e316d2d053271b64593a7360de19acfc1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+// Write your solution in this fileconst customerName = 'bob'
+var customerName = 'bob'
+console.log(customerName)
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'tom'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+
+}
